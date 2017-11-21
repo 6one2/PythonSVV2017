b9cbee003491443aea30661852bb317461c95830
Sebastien Villard <sebastien.villard@gmail.com>
2017-11-21 16:05:07 -0500

    Add: detect transition and cut into section + correlation with Sinus for each section + plot each section

--- TestVoltMF_test.py
+++ TestVoltMF_test.py
@@ -42,24 +42,35 @@ for fname in datafile:
     ## find transition of B.
     thr=2.5
     ii = np.flatnonzero(np.diff(data.MF[idxPeak])>thr)#look for transition in max MF
-    idxGP=idxPeak(ii)+1
+    idxSP=idxPeak[ii+1];
+    idxEP=idxPeak[ii[1:]];idxEP = np.append(idxEP,idxPeak[-1])
+    
+    for st, en in zip(idxSP, idxEP):
+        section = np.arange(st,en)
+        
+        A = data.MF[st]
+        w = 20
+        t = data.time[section]-data.time[st]
+        Sin = A*np.sin(2*np.pi*w*t+np.pi/2)
+        
+        COR = np.corrcoef(Sin,data.MF[section])[0][1]
+        
+        plt.figure()
+        
+        plt.plot(data.time[section],data.MF[section],linewidth=3)
+        plt.plot(data.time[section],Sin,'k')
+        plt.ylim(-210,210)
+        plt.title('R = ' + str(COR))
     
     # check np.polyfit for linear regression
+    
 
     #graph  
-    if True:
+    if False:
 #        plt.figure(0)
 #        plt.plot(data.time,data.MF)
 #        plt.plot(data.time,MFf,'k--',linewidth=1)
         
-        plt.figure(1)
-        plt.plot(data.time,data.MF)
-        plt.plot(data.time[idxPeak], data.MF[idxPeak],'ko')
-        plt.plot(data.time[idxGP], data.MF[idxGP],'ro')
-        
-        plt.figure(3)
-        PP = plt.plot(np.diff(data.MF[idxPeak]),'.-',markercolor='k')
-        plt.plot(np.diff(data.MF[idxPeak]),'.-')
         
     
 #old code
